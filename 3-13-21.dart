2a8a0b216b72de291f95e4e0aa27d1c7925e92b1
Oindril Dutta <duttaoindril@gmail.com>
2021-03-15 23:21:31 +0000

    finished 3/13 challenge

--- 3-13-21.dart
+++ 3-13-21.dart
@@ -1,51 +1,41 @@
-class Node<T> {
-  Node<T>? next;
-  final T value;
-
-  Node(this.value, [this.next]);
-
-  Node<T> setNext(Node<T> nextNode) {
-    next = nextNode;
-    return nextNode;
-  }
-
-  @override
-  String toString() {
-    return 'Node($value) -> ${next}';
-  }
-
-  int get listValue {
-    final int? nextValue = next?.listValue;
-    return int.parse('${nextValue == null ? '' : nextValue}$value');
-  }
-
-  String get printed {
-    return '${toString()} ==> ${listValue}';
-  }
+int lengthOfLongestSubstring(String s) {
+  List<String> splitStr = s.split('');
+  Set<String> lastSet = <String>{};
+  int largestSize = 1;
+  int pointerA = 0;
+  int pointerB = 0;
+  do {
+    if(pointerA == pointerB) {
+      lastSet.add(splitStr.elementAt(pointerA));  
+      pointerB += 1;
+    } else {
+      final currentLetter = splitStr.elementAt(pointerB);
+      if(lastSet.contains(currentLetter)) {
+        if(lastSet.length > largestSize) {
+          largestSize = lastSet.length;
+        }
+        pointerA = splitStr.indexOf(currentLetter, pointerA) + 1;
+        pointerB = pointerA;
+        lastSet = <String>{};
+      } else {
+        pointerB += 1;
+        lastSet.add(currentLetter);
+      }
+    }
+  } while(
+    pointerA < splitStr.length - 1 && pointerB < splitStr.length
+  );
+  return largestSize;
 }
 
-Node<int>? sum(Node<int>? a, Node<int>? b, [bool carry = false]) {
-  if (a == null && b == null) {
-    return null;
-  }
-  final int firstA = a?.value ?? 0;
-  final int firstB = b?.value ?? 0;
-  final int added = firstA + firstB + (carry ? 1 : 0);
-  final bool nextCarry = added >= 10;
-  final Node<int> finalToAdd = Node<int>(nextCarry ? added - 10 : added);
-  final Node<int>? nextNode = sum(a?.next, b?.next, nextCarry);
-  print(nextNode);
-  return nextNode == null ? finalToAdd : (finalToAdd..setNext(nextNode));
+void test(String test, int Function(String) func, int solution) {
+  int result = func(test);
+  bool passed = solution == result;
+  print(
+    'Test case $test ${passed ? 'PASSED' : 'FAILED'} for solution $solution with result $result.',
+  );
 }
 
 void main() {
-  final Node<int> listA = Node(9)
-    ..setNext(Node(2)..setNext(Node(4)..setNext(Node(3))));
-  final Node<int> listB = Node(5)..setNext(Node(6)..setNext(Node(4)));
-
-  print(listA.printed);
-  print(listB.printed);
-
-  final Node<int>? summed = sum(listA, listB);
-  print(summed?.printed);
+  test('abrkaabcdefghijjxxx', lengthOfLongestSubstring, 10);
 }
